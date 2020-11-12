1f2ac0e9ce03840c4461fdd373a19006230c845a
Alex Williams <alexjwilliams1@gmail.com>
2020-11-11 16:47:43 -0800

    added sass

--- src/components/common/CityCard.js
+++ src/components/common/CityCard.js
@@ -11,7 +11,9 @@ function formatCurrency(num) {
 const CityCard = props => {
   return (
     <div className="city-card">
-      <h2>{props.city.cityName}</h2>
+      <div className="city-card-header">
+        <h2>{props.city.cityName}</h2>
+      </div>
       <p>Population: {formatLongNum(props.city.population)}</p>
       <p>Avg. Rent (1BR): {formatCurrency(props.city.rentRate)}</p>
       <p>Median Income: {formatCurrency(props.city.medIncome)}</p>
