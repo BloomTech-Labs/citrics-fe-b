5d808d5b388abe83c7b8217e2b7436fd7bf19470
Alex Williams <alexjwilliams1@gmail.com>
2020-11-18 13:13:54 -0800

    merged

--- archive/src/state/actions/index.js
+++
@@ -1,7 +0,0 @@
-import * as searchBarActs from './searchBarActs';
-import * as cityCardActs from './cityCardActs';
-import * as cardContainerActs from './cardContainerActs';
-import * as themeActs from './themeActs';
-import * as userActs from './userActs';
-
-export { themeActs, searchBarActs, cardContainerActs, cityCardActs, userActs };
