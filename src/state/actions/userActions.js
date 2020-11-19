817fed90ef49f108784ae91c4f06bd0431663b9f
karina-rodriguez <karminer60@gmail.com>
2020-11-18 20:25:37 -0800

    adding addFavorite and removeFavorite actions to store and reworking  toggleFavorite to get actions from store

--- src/state/actions/userActions.js
+++ src/state/actions/userActions.js
@@ -1,8 +1,34 @@
 import axios from 'axios'
 
-export const ADD_FAVORITE = 'ADD_FAVORITE'
-export const REMOVE_FAVORITE = 'REMOVE_FAVORITE'
+export const ADD_FAVORITE_START = 'ADD_FAVORITE_START'
+export const ADD_FAVORITE_SUCCESS = 'ADD_FAVORITE_SUCCESS'
+export const ADD_FAVORITE_FAILURE = 'ADD_FAVORITE_FAILURE'
 
-export const addFavorite = () => dispatch => {
-  dispatch({ type: ADD_FAVORITE })
+export const REMOVE_FAVORITE_START = 'ADD_FAVORITE_START'
+export const REMOVE_FAVORITE_SUCCESS = 'ADD_FAVORITE_SUCCESS'
+export const REMOVE_FAVORITE_FAILURE = 'ADD_FAVORITE_FAILURE'
+
+export const addFavorite = cityId => dispatch => {
+  dispatch({ type: ADD_FAVORITE_START })
+  axios
+    .post(`https://labs-28-citrics-b.herokuapp.com/cities/favorite/${cityId}`)
+    .then(res => {
+      dispatch({ type: ADD_FAVORITE_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: ADD_FAVORITE_FAILURE, payload: err.message })
+    })
+}
+
+
+export const removeFavorite = cityId => dispatch => {
+  dispatch({ type: REMOVE_FAVORITE_START })
+  axios
+    .delete(`https://labs-28-citrics-b.herokuapp.com/cities/favorite/${cityId}`)
+    .then(res => {
+      dispatch({ type: REMOVE_FAVORITE_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: REMOVE_FAVORITE_FAILURE, payload: err.message })
+    })
 }
